b508deafd96e2a876513a51f9522802fbf61e0d3
Paulo Santiago <pauloecsantiago@gmail.com>
2022-12-09 17:45:34 +0000

    Traversing the DOM. Not working

--- app.js
+++ app.js
@@ -1,2 +1,15 @@
-//using selectors inside the element
 // traversing the dom
+//selection al the buttons
+//question-btn
+
+const btns = document.querySelectorAll(".question-btn");
+
+// NOT WORKING:
+/* btns.forEach((btn) => {
+  btn.addEventListener("click", (event) => {
+    const question = event.currentTarget.parentElement.parentElement;
+    question.classList.toggle("show-text");
+  });
+}); */
+
+//using selectors inside the element
