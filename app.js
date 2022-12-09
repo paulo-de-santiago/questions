17a5b37332e79efb32dfebd42809f16aa7486052
Paulo Santiago <pauloecsantiago@gmail.com>
2022-12-09 17:57:25 +0000

    Using selectors inside the element to change elements.

--- app.js
+++ app.js
@@ -1,15 +1,30 @@
-// traversing the dom
+//using selectors inside the element
 //selection al the buttons
 //question-btn
 
-const btns = document.querySelectorAll(".question-btn");
+const questions = document.querySelectorAll(".question");
+
+questions.forEach(function (question) {
+  //console.log(question);
+  const btn = question.querySelector(".question-btn");
+  //  console.log(btn);
+
+  // add eventListener to button so question will toggle
+
+  btn?.addEventListener("click", function () {
+    question.classList.toggle("show-text");
+  });
+});
 
 // NOT WORKING:
-/* btns.forEach((btn) => {
+// traversing the dom
+
+/*
+const btns = document.querySelectorAll(".question-btn");
+
+btns.forEach((btn) => {
   btn.addEventListener("click", (event) => {
     const question = event.currentTarget.parentElement.parentElement;
     question.classList.toggle("show-text");
   });
 }); */
-
-//using selectors inside the element
